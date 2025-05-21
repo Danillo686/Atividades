ac1156db431df16723e7266d9840a113cb167d74
Danillo686 <uqualquer220@gmail.com>
2025-05-20 21:55:48 -0300

    Create Aula 12.js

---
+++ Apredendo JS/Aula 12.js
@@ -0,0 +1,7 @@
+var vel = 90
+console.log (`A velocidade é de ${vel}`)
+if (vel > 70){
+    console.log ("Você ultrapassou a velocidade, você foi multado")
+} else if (vel == 60){
+console.log ("Dirija com segurança")
+}
